1bd209ccef217b8a1a08170e14717b6a53ed2cca
Daniel F <danielf@openmailbox.org>
2015-12-18 22:19:00 -0800

    small changes

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+/*
+ * scratchblock
+ * https://github.com/etchcode/scratchblock
+ *
+ * Copyright (c) 2015 Daniel Franklin and Sam Schickler
+ * Licensed under the MIT license.
+ */
+
+module.exports = function (grunt) {
+    'use strict';
+
+    grunt.loadTasks('tasks');
+    
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js',
+            ],
+        },
+
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            tests: ['tmp']
+        },
+
+        // Configuration to be run (and then tested).
+        scratchblock: {
+            generate: {
+                options: {},
+                files: [
+                    {expand: true, src: 'test/*.html', dest: "test/"}
+                ]
+            }
+        }
+    });
+
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
+    grunt.registerTask('test', ["clean", "scratchblock"]);
+
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'test']);
+};
